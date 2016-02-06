034d184a82d2ef175505598d3186c8e1a02cdb7f
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2016-02-06 10:53:42 +0100

    project setup

---
+++ android/src/com/matvidako/puzzle/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.matvidako.puzzle;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.matvidako.puzzle.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ desktop/src/com/matvidako/puzzle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.matvidako.puzzle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.matvidako.puzzle.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
